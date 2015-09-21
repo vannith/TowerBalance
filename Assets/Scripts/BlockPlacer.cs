3d52048ad275a3ddbed3b230c45d77452e415664
Paximillian <morzil@walla.com>
2015-09-21 18:03:50 +0300

    Fixed resolution bug with camera vertical movement.

--- Assets/Scripts/BlockPlacer.cs
+++ Assets/Scripts/BlockPlacer.cs
@@ -8,7 +8,6 @@ public class BlockPlacer : MonoBehaviour
     private const KeyCode k_PlacementButton = KeyCode.Space;
     private const string k_BlockTag = "Block";
     private const string k_CameraLimitLayer = "CameraLimits";
-    private const int k_HeightOfBlockInPixels = 32;
 
     private Rigidbody2D m_HeldBlock = null;
     private bool m_WaitingOnNextBlock = true;
@@ -16,69 +15,63 @@ public class BlockPlacer : MonoBehaviour
     private float m_RightBorderHorizontalPosition;
     private float m_LeftBorderHorizontalPosition;
     private int m_LandedBlocks = 0;
+    private bool m_ReadyToPlay = false;
+    private int m_HeightOfBlockInPixels;
+    private int m_WidthOfBlockInPixels;
 
     private Coroutine m_PlacerMover;
     
     [SerializeField]
     private float m_TimeBetweenBlocks;
     [SerializeField]
-    private AnimationCurve m_SpeedPerLandedBlocks;
-
-
+    private AnimationCurve m_SideToSideTimePerLandedBlocks;
+    
     // Use this for initialization
     void Start()
     {
         m_HeldBlock = ObjectPoolManager.PullObject(k_BlockTag).GetComponent<Rigidbody2D>();
-        m_HeldBlock.transform.position = transform.position;
+        m_HeldBlock.GetComponent<Collider2D>().enabled = false;
 
-        //Checks if we have any camera limits set up correctly
-        RaycastHit2D hit = Physics2D.RaycastAll(transform.position, Vector2.right).First((i_Hit) => i_Hit.transform.gameObject.layer == LayerMask.NameToLayer(k_CameraLimitLayer));
-        if (!hit)
-        {
-            Debug.LogError("No camera limits found. Please set up the camera limits correctly(As childs of the camera and on the edges of it, giving them the CameraLimits layer).");
-        }
-        else
-        {
-            m_HorizontalDropPosition = m_RightBorderHorizontalPosition = hit.transform.position.x - m_HeldBlock.transform.localScale.x;
-        }
+        SpriteRenderer sprite = m_HeldBlock.GetComponent<SpriteRenderer>();
+        m_HeightOfBlockInPixels = sprite.sprite.texture.height;
+        m_WidthOfBlockInPixels = sprite.sprite.texture.width;
 
-        hit = Physics2D.RaycastAll(transform.position, Vector2.left).First((i_Hit) => i_Hit.transform.gameObject.layer == LayerMask.NameToLayer(k_CameraLimitLayer));
-        if (!hit)
-        {
-            Debug.LogError("No camera limits found. Please set up the camera limits correctly(As childs of the camera and on the edges of it, giving them the CameraLimits layer).");
-        }
-        else
-        {
-            m_LeftBorderHorizontalPosition = hit.transform.position.x + m_HeldBlock.transform.localScale.x;
-        }
-
+        Camera.main.orthographicSize = Camera.main.pixelHeight / sprite.sprite.pixelsPerUnit / 2;
+        StartCoroutine(scrollUpForGameStart());
+        
+        m_HorizontalDropPosition = m_RightBorderHorizontalPosition = Camera.main.ScreenToWorldPoint(new Vector3(Screen.width - m_WidthOfBlockInPixels / 2, Screen.height / 2, transform.position.z)).x;
+        m_LeftBorderHorizontalPosition = Camera.main.ScreenToWorldPoint(new Vector3(0  + m_WidthOfBlockInPixels / 2, Screen.height / 2, transform.position.z)).x;
+       
         m_PlacerMover = StartCoroutine(movePlacerPosition());
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (m_HeldBlock)
+        if (m_ReadyToPlay)
         {
-            m_HeldBlock.transform.position = new Vector2(m_HorizontalDropPosition, transform.position.y);
+            if (m_HeldBlock)
+            {
+                m_HeldBlock.transform.position = new Vector2(m_HorizontalDropPosition, transform.position.y);
 
 #if UNITY_EDITOR || UNITY_STANDALONE
-            if (Input.GetKeyDown(k_PlacementButton))
-            {
-                dropBlock();
-            }
+                if (Input.GetKeyDown(k_PlacementButton))
+                {
+                    dropBlock();
+                }
 #elif UNITY_ANDROID || UNITY_IOS || UNITY_BLACKBERRY || UNITY_WP8 || UNITY_TIZEN
             if (Input.touchCount > 0 && Input.GetTouch(0).phase == TouchPhase.Began)
             {
                 dropBlock();
             }
 #endif
-        }
-        else
-        {
-            if (m_WaitingOnNextBlock)
+            }
+            else
             {
-                StartCoroutine(readyNextBlock());
+                if (m_WaitingOnNextBlock)
+                {
+                    StartCoroutine(readyNextBlock());
+                }
             }
         }
     }
@@ -98,10 +91,15 @@ public class BlockPlacer : MonoBehaviour
     
     private IEnumerator movePlacerPosition()
     {
+        while (!m_ReadyToPlay)
+        {
+            yield return null;
+        }
+
         bool movingLeft = true;
         
         float timeAtStart = Time.time;
-        float timeAtFinish = timeAtStart + m_SpeedPerLandedBlocks.Evaluate(m_LandedBlocks);
+        float timeAtFinish = timeAtStart + m_SideToSideTimePerLandedBlocks.Evaluate(m_LandedBlocks);
          
         while (true)
         {
@@ -112,7 +110,7 @@ public class BlockPlacer : MonoBehaviour
             if (percentComplete >= 1)
             {
                 timeAtStart = Time.time;
-                timeAtFinish = timeAtStart + m_SpeedPerLandedBlocks.Evaluate(m_LandedBlocks);
+                timeAtFinish = timeAtStart + m_SideToSideTimePerLandedBlocks.Evaluate(m_LandedBlocks);
                 movingLeft = !movingLeft;
             }
 
@@ -130,15 +128,28 @@ public class BlockPlacer : MonoBehaviour
         m_PlacerMover = StartCoroutine(movePlacerPosition());
     }
 
-    private IEnumerator scrollUp1Line()
+    private IEnumerator scrollUpForGameStart()
+    {
+        for (int i = 0; i < Mathf.RoundToInt(Camera.main.orthographicSize) - 1; ++i)
+        {
+            yield return StartCoroutine(scrollUp1Line(0.5f));
+
+            yield return null;
+        }
+
+        m_ReadyToPlay = true;
+        m_HeldBlock.GetComponent<Collider2D>().enabled = true;
+    }
+
+    private IEnumerator scrollUp1Line(float i_TimePerScroll = 1)
     {
         float timeAtStart = Time.time;
-        float timeAtFinish = timeAtStart + 1;
+        float timeAtFinish = timeAtStart + i_TimePerScroll;
         float percentComplete = 0;
 
         float startingVerticalPosition = transform.position.y;
         Vector3 currentScreenPosition = Camera.main.WorldToScreenPoint(transform.position);
-        Vector3 targetWorldPosition = Camera.main.ScreenToWorldPoint(currentScreenPosition + Vector3.up * k_HeightOfBlockInPixels);
+        Vector3 targetWorldPosition = Camera.main.ScreenToWorldPoint(currentScreenPosition + Vector3.up * m_HeightOfBlockInPixels);
         float endingVerticalPosition = targetWorldPosition.y;
 
         do
